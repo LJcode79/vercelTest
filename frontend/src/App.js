2cf02e41fcc4b5496d3edd1ae16afde8a7acaa18
LJ <ljdb1999@gmail.com>
2024-01-19 10:25:35 -0800

    14th push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [backendData, setBackendData] = useState([{}])
 
   useEffect(() => {
-    fetch("https://vercel-test-server-gamma.vercel.app/api").then(
+    fetch(process.env.REACT_APP_SERVER + "/api").then(
       response => response.json()
     ).then(
       data => {
